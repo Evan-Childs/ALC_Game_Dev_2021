bc4b11b2455d839ace60c3d89c3a816f16c981b1
Evan <evan.c405@stu.nebo.edu>
2021-09-21 14:21:25 -0600

    Added better movement and boundaries

--- Prototype 2 - Top Down Shooter/Assets/Scripts/EnemyWallCollision.cs
+++ Prototype 2 - Top Down Shooter/Assets/Scripts/EnemyWallCollision.cs
@@ -2,29 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class EnemyWallCollision : MonoBehaviour
 {
-    public float speed = 10.00f;
-    public float turnSpeed = 10.00f;
-    private float vInput;
-    private float hInput;
-    
-    public float xRange = 9.98f;
-    public float yRange = 4.5f;
+
+    public float xRange = 9.73f;
+    public float yRange = 4.23f;
 
     // Update is called once per frame
     void Update()
     {
-        //Gathering Keyboard Input(Movement)
-        hInput = Input.GetAxis("Horizontal");
-        vInput = Input.GetAxis("Vertical");
-
-        //Moves Player Character
-        transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
-        //Rotates Player Character
-        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * hInput);
-        //creates left and right walls
-        if(transform.position.x > xRange)
+                if(transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
         }
